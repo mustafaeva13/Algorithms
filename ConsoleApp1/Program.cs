31a811ed3f144063be1d3bc6dc4bfcb5a432face
efsane <126017552+mustafaeva13@users.noreply.github.com>
2023-03-29 19:32:31 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+namespace lesson3
+{
+    class Program
+    {
+        
+       
+        static void Main(string[] args)
+        {
+            //Task 2
+            // Master(39);
+
+            //    Gamer("developer");
+
+
+            /// Tasks3
+            //int a,b,sum=0;
+            //Console.Write("enter your number:");
+            //a = int.Parse(Console.ReadLine());
+            //while (a > 0)
+            //{
+            //    b = a % 10;
+            //    sum = sum +b;
+            //    a=a/ 10;
+
+            //}
+            //Console.Write("sum=" + sum);
+
+            ///Task5
+            //static void Gamer(string name)
+            //{
+            //    Console.WriteLine( name);
+            //}
+
+
+            //Task1
+            //static void Master(int number) {
+            //int n;
+            //Console.WriteLine("enter your name: ");
+            //n = int.Parse(Console.ReadLine());
+            //int num1 = n % 10;
+            //int num2 = n;
+            //while (num1 >= 10)
+            //{
+            //    num1 /= 10;
+            //}
+            //Console.WriteLine("1st num=" + num1);
+            //number /= 10;
+            //Console.WriteLine(number);
+
+            //int n, r, sum = 0,temp;
+            //Console.Write("Enter the Number: ");
+            //n = int.Parse(Console.ReadLine());
+            //temp = n;
+            //while (n > 0)
+            //{
+            //    r = n % 10;
+            //    sum = (sum * 10) + r;
+            //    n = n / 10;
+            //}
+            //if (temp == sum)
+            //    Console.Write("Number is Palindrome.");
+            //else
+            //    Console.Write("Number is not Palindrome");
+
+
+
+        }
+
+
+        }
+    }
+
